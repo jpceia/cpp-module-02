746233fc7d6938cb085e04fbc599ffbcdfda3457
jpceia <joao.p.ceia@gmail.com>
2021-11-02 22:23:36 +0000

    added comments

--- ex02/headers/Fixed.hpp
+++ ex02/headers/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 19:00:54 by jceia             #+#    #+#             */
-/*   Updated: 2021/11/02 21:15:22 by jceia            ###   ########.fr       */
+/*   Updated: 2021/11/02 22:22:34 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,32 @@
 class Fixed
 {
 private:
-    /* data */
     int _raw;
     static const int fracBits = 8;
+
 public:
+
+    // Constructors
     Fixed(void);
     Fixed(const int value);
     Fixed(const float value);
     Fixed(const Fixed& x);
+
+    // Destructor
     ~Fixed(void);
 
+    // Assignment operator
     Fixed& operator = (const Fixed& x);
 
+    // Get-Set
     int getRawBits(void) const;
     void setRawBits(int const raw);
+
+    // Converters
     float toFloat(void) const;
     int toInt(void) const;
 
-    // comparison operators
+    // Comparison operators
     bool operator> (const Fixed& x);
     bool operator< (const Fixed& x);
     bool operator>= (const Fixed& x);
@@ -50,17 +58,19 @@ public:
     Fixed operator/ (const Fixed& x);
 
     // Increment/Decrement operators
-    Fixed& operator++(void);
-    Fixed operator++(int);
-    Fixed& operator--(void);
-    Fixed operator--(int);
+    Fixed& operator++(void);    // Pre-Increment
+    Fixed operator++(int);      // Post-Increment
+    Fixed& operator--(void);    // Pre-Decrement
+    Fixed operator--(int);      // Post-Decrement
 
+    // Min-Max
     static Fixed& min(Fixed& x, Fixed& y);
     static const Fixed& min(const Fixed& x, const Fixed& y);
     static Fixed& max(Fixed& x, Fixed& y);
     static const Fixed& max(const Fixed& x, const Fixed& y);
 };
 
+// Ostream operator
 std::ostream& operator << (std::ostream &out, const Fixed& x);
 
 #endif
